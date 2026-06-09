e3fd2790fe371087d883bc54a7a8c3def403689c
muhammadtalha <talha.c@pentabitlabs.com>
2026-06-09 17:19:41 +0500

    Aks Core updated

--- aks/build.gradle.kts
+++ aks/build.gradle.kts
@@ -21,8 +21,8 @@ publishing {
         create<MavenPublication>("AppsKitSDK") {
             groupId = "com.github.Pentabit-Labs-LLC"
             artifactId = "AppsKitSDK-core"
-            version = "6.2.0.0"
-            artifact("$projectDir/libs/AppsKitSDK_v6200.aar")
+            version = "6.3.0.0"
+            artifact("$projectDir/libs/AppsKitSDK_v6300.aar")
         }
 
         create<MavenPublication>("AppsKitSDKSupport") {
