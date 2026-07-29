29abe2845125784551a32ee07c4c57fc705b5514
muhammadtalha <talha.c@pentabitlabs.com>
2026-07-29 15:50:30 +0500

    Native Ad Dynamic Layouts

--- aks/build.gradle.kts
+++ aks/build.gradle.kts
@@ -21,8 +21,8 @@ publishing {
         create<MavenPublication>("AppsKitSDK") {
             groupId = "com.github.Pentabit-Labs-LLC"
             artifactId = "AppsKitSDK-core"
-            version = "6.3.3.1"
-            artifact("$projectDir/libs/AppsKitSDK_v6331.aar")
+            version = "7.0.0.0"
+            artifact("$projectDir/libs/AppsKitSDK_v7000.aar")
         }
 
         create<MavenPublication>("AppsKitSDKSupport") {
