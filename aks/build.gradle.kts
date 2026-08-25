d0ef6a00a4e0765ed0440f7587c3677c9d06900d
muhammadtalha <talha.c@pentabitlabs.com>
2026-08-25 16:22:08 +0500

    SolarEngine Revenue Reporting fixed

--- aks/build.gradle.kts
+++ aks/build.gradle.kts
@@ -21,8 +21,8 @@ publishing {
         create<MavenPublication>("AppsKitSDK") {
             groupId = "com.github.Pentabit-Labs-LLC"
             artifactId = "AppsKitSDK-core"
-            version = "7.0.0.4"
-            artifact("$projectDir/libs/AppsKitSDK_v7004.aar")
+            version = "7.0.1.0"
+            artifact("$projectDir/libs/AppsKitSDK_v7010.aar")
         }
 
         create<MavenPublication>("AppsKitSDKSupport") {
