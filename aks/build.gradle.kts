a047a493110005c2ee5017ffc2df8637ef606e75
muhammadtalha <talha.c@pentabitlabs.com>
2026-09-25 17:35:51 +0500

    AKS Core Paywall bug fix

--- aks/build.gradle.kts
+++ aks/build.gradle.kts
@@ -21,8 +21,8 @@ publishing {
         create<MavenPublication>("AppsKitSDK") {
             groupId = "com.github.Pentabit-Labs-LLC"
             artifactId = "AppsKitSDK-core"
-            version = "8.0.0.6"
-            artifact("$projectDir/libs/AppsKitSDK_v8006.aar")
+            version = "8.0.0.7"
+            artifact("$projectDir/libs/AppsKitSDK_v8007.aar")
         }
 
         create<MavenPublication>("AppsKitSDKSupport") {
